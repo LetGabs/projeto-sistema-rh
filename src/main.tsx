c1c9f6bc40926addba87833adbc7ce4f4a384cc0
Letícia Gabrielly da Silva <165218856+LetGabs@users.noreply.github.com>
2025-11-24 20:07:23 -0300

    att deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,15 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import "./style/estilo.css";
 
-
-
 const rootElement = document.getElementById("root");
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
     <React.StrictMode>
-      <BrowserRouter>
+      <BrowserRouter basename="/projeto-sistema-rh">
         <App />
       </BrowserRouter>
     </React.StrictMode>
